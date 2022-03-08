907807b55393b1e8dcbbdbded6d5f50cb7898fc1
PlunderS11 <99624571+PlunderS11@users.noreply.github.com>
2022-03-08 09:18:55 +0700

    Add files via upload
    tang chi

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Thảo My';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100018493332325';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Yêu chị';
+const contentCard = ' Chúc chị 1 ngày 8-3 vui vẻ, hạnh phúc, tràn đầy niềm vui và cười thật nhiều, thật tươi. Nhưng đối với em thì không riêng ngày 8-3 mà chúc chị cả 354 ngày còn lại.💘';
+
+const giftImage = 'My.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
